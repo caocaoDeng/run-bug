330bad73c97ccdfe7415c330a2521b7fd441a61d
wb_dengwei05 <wb_dengwei05@meituan.com>
2025-10-11 17:42:23 +0800

    complate

--- app.js
+++ app.js
@@ -3,27 +3,49 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 
 const app = express()
+const url = 'https://www.xiaohongshu.com'
 
 const getData = async () => {
   const notes = []
-  const res = await axios.get('https://www.xiaohongshu.com/explore')
-  const jq = cheerio.load(res.data)
-  jq('#exploreFeeds .note-item>div').each((_, elm) => {
-    const childs = elm.children.filter(item => !!item.name).slice(1)
-    childs.forEach(item => {
-      console.log(jq(item).attr('href'))
-    })
+  const res = await axios.get(`${url}/explore`)
+  const $ = cheerio.load(res.data)
+  const elms = $('#exploreFeeds .note-item>div')
+  elms.each((_, elm) => {
+    const cover = $(elm).find('.cover')
+    const footer = $(elm).find('.footer')
     notes.push({
-      link: jq(childs[1]).attr('href'),
-      cover: jq(childs[1].children[0]).attr('src'),
-      // title: jq(childs[2].children[0].children[0]).text(),
+      link: $(cover).attr('href'),
+      cover: $(cover).find('img').attr('src'),
+      title: $(footer).find('.title span').text(),
+      avatar: $(footer)
+        .find('.author-wrapper .author .author-avatar')
+        .attr('src'),
+      author: $(footer).find('.author-wrapper .author .name').text(),
+      like: $(footer).find('.author-wrapper .like-wrapper .count').text(),
     })
   })
-  return notes
+  return notes.map((item, index) => ({ ...item, _id: index }))
+}
+
+// https://www.xiaohongshu.com/explore/68ce0892000000001300621f?xsec_token=ABIltKV-AuNX6cWoFVYVupB1v6iNj6dyRGmvroG6th_RA=&xsec_source=pc_feed
+const getDetail = async ({ link }) => {
+  const imgs = []
+  const res = await axios.get(
+    'https://www.xiaohongshu.com/explore/68ce0892000000001300621f?xsec_token=ABIltKV-AuNX6cWoFVYVupB1v6iNj6dyRGmvroG6th_RA=&xsec_source=pc_feed'
+  )
+  const $ = cheerio.load(res.data)
+  const elms = $('.swiper-wrapper .swiper-slide')
+  console.log(elms.length)
+  elms.each((_, elm) => {
+    const src = $(elm).find('.img-container .note-slider-img').attr('src')
+    imgs.push(src)
+  })
+  return imgs
 }
 
 app.get('/', async (req, res) => {
   const notes = await getData()
+  // const imgs = await getDetail(notes[0])
   res.send(notes)
 })
 
